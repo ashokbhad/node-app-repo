82552c50b39292e6e982cb92ba66dc748e3b2633
ashok-bhad <ashokbhad321@gmail.com>
2024-09-17 16:12:12 +0530

    Built in Module OS & global objects
    Built in Module OS & global objects

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # node-app-repo
 This is Node APP Repository
+
+# project build steps
+1. clone the repository - <https://github.com/ashokbhad/node-app-repo>
+2. npm install or npm update
+
+# Author
+Ashok Bhad
+Mobile No: 8605703018
---
+++ index.js
@@ -0,0 +1,24 @@
+const os = require("os");
+console.log("My first app");
+console.log(`File Name: ${__filename}`);
+console.log(`Directory Name: ${__dirname}`);
+
+console.log("=============== Built in Module - OS =======================");
+console.log(`Arch: ${os.arch()}`);
+console.log(`cpus: ${os.cpus()}`);
+console.log(`freemem: ${os.freemem()}`);
+console.log(`totalmem: ${os.totalmem()}`);
+console.log(`hostname: ${os.hostname()}`);
+console.log(`platform: ${os.platform()}`);
+console.log(`release: ${os.release()}`);
+console.log(`type: ${os.type()}`);
+console.log(`uptime: ${os.uptime()}`);
+console.log(`userInfo: ${os.userInfo()}`);
+
+console.log("%c====== Global Objects in JS =========");
+console.log(global);
+console.log(console);
+console.log(process);
+console.log(__filename);
+console.log(__dirname);
+console.log(module);
