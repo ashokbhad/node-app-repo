f15a5a571e46ae72b9f7211e41163a890285654f
ashok-bhad <ashokbhad321@gmail.com>
2024-09-20 21:09:22 +0530

    os,path,url modules
    os,path,url modules

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const os = require("os");
+
 console.log("My first app");
 console.log(`File Name: ${__filename}`);
 console.log(`Directory Name: ${__dirname}`);
 
-console.log("=============== Built in Module - OS =======================");
+console.log(
+  "%c=============== Built in Module - OS =======================",
+  "color: green"
+);
 console.log(`Arch: ${os.arch()}`);
 console.log(`cpus: ${os.cpus()}`);
 console.log(`freemem: ${os.freemem()}`);
@@ -15,7 +19,7 @@ console.log(`type: ${os.type()}`);
 console.log(`uptime: ${os.uptime()}`);
 console.log(`userInfo: ${os.userInfo()}`);
 
-console.log("%c====== Global Objects in Node JS =========");
+console.log("%c====== Global Objects in Node JS =========", "color: green");
 console.log(global);
 console.log(console);
 console.log(process);
